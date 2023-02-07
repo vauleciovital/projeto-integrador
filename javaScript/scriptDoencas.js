5eecfcb9337693df5f2b34f6619c33d5c3e9bd27
vauleciovital <89229895+vauleciovital@users.noreply.github.com>
2023-02-07 15:58:06 -0300

    Add files via upload

---
+++ javaScript/scriptDoencas.js
@@ -0,0 +1,19 @@
+
+function doenca_um() {
+  var text_um = document.getElementById("text_um");
+  if (text_um.style.display === "none") {
+    text_um.style.display = "block";
+  } else {
+    text_um.style.display = "none";
+  }
+}
+
+
+function doenca_dois() {
+    var text_dois = document.getElementById("text_dois");
+    if (text_dois.style.display === "none") {
+      text_dois.style.display = "block";
+    } else {
+      text_dois.style.display = "none";
+    }
+  }
