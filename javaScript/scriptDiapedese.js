aa5a87ea5ee055123dc1354786aee0e8c64c9e60
vauleciovital <89229895+vauleciovital@users.noreply.github.com>
2023-02-07 21:43:27 -0300

    coiso - isa

--- javaScript/scriptDiapedese.js
+++ javaScript/scriptDiapedese.js
@@ -1,1 +1,27 @@
+function fase_um() {
+    var text_faseum = document.getElementById("text_faseum");
+    if (text_faseum.style.display === "none") {
+        text_faseum.style.display = "block";
+    } else {
+        text_faseum.style.display = "none";
+    }
+}
 
+
+function fase_dois() {
+    var text_fasedois = document.getElementById("text_fasedois");
+    if (text_fasedois.style.display === "none") {
+        text_fasedois.style.display = "block";
+    } else {
+        text_fasedois.style.display = "none";
+    }
+}
+
+function fase_tres() {
+    var text_fasetres = document.getElementById("text_fasetres");
+    if (text_fasetres.style.display === "none") {
+        text_fasetres.style.display = "block";
+    } else {
+        text_fasetres.style.display = "none";
+    }
+}
