7ee03c4e9bd087d6be271cf25abca3227f972531
vauleciovital <89229895+vauleciovital@users.noreply.github.com>
2023-02-07 21:36:06 -0300

    renomear nome arquivo

---
+++ javaScript/scriptsEndocitose.js
@@ -0,0 +1,27 @@
+function primeiraParte() {
+    var text_um = document.getElementById("primeiroTexto");
+    if (text_um.style.display === "none") {
+      text_um.style.display = "block";
+    } else {
+      text_um.style.display = "none";
+    }
+  }
+  
+  
+  function segundaParte() {
+      var text_dois = document.getElementById("segundoTexto");
+      if (text_dois.style.display === "none") {
+        text_dois.style.display = "block";
+      } else {
+        text_dois.style.display = "none";
+      }
+    }
+    
+  function terceiraParte() {
+      var text_tres = document.getElementById("terceiroTexto");
+      if (text_tres.style.display === "none") {
+        text_tres.style.display = "block";
+      } else {
+        text_tres.style.display = "none";
+      }
+    }
