ff5988fd42bc531e1baa7d08ddd2b71b7da61e17
vauleciovital <89229895+vauleciovital@users.noreply.github.com>
2023-02-08 08:41:36 -0300

    ultimo js - isa

---
+++ javaScript/scriptLeucocitos.js
@@ -0,0 +1,44 @@
+function tipo1() {
+    var text_tipo1 = document.getElementById("text_tipo1");
+    if (text_tipo1.style.display === "none") {
+        text_tipo1.style.display = "block";
+    } else {
+        text_tipo1.style.display = "none";
+    }
+}
+
+function tipo2() {
+    var text_tipo2 = document.getElementById("text_tipo2");
+    if (text_tipo2.style.display === "none") {
+        text_tipo2.style.display = "block";
+    } else {
+        text_tipo2.style.display = "none";
+    }
+}
+
+function tipo3() {
+    var text_tipo3 = document.getElementById("text_tipo3");
+    if (text_tipo3.style.display === "none") {
+        text_tipo3.style.display = "block";
+    } else {
+        text_tipo3.style.display = "none";
+    }
+}
+
+function tipo4() {
+    var text_tipo4 = document.getElementById("text_tipo4");
+    if (text_tipo4.style.display === "none") {
+        text_tipo4.style.display = "block";
+    } else {
+        text_tipo4.style.display = "none";
+    }
+}
+
+function tipo5() {
+    var text_tipo5 = document.getElementById("text_tipo5");
+    if (text_tipo5.style.display === "none") {
+        text_tipo5.style.display = "block";
+    } else {
+        text_tipo5.style.display = "none";
+    }
+}
